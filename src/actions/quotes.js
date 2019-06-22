268143e713aac4010861aab0070a25d29141bf9d
Daniel Hallada <hallada.daniel@gmail.com>
2019-06-21 20:17:29 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,29 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+    return {
+        type:"ADD_QUOTE",
+        quote
+    }
+}
+
+export const removeQuote = (quoteToRemove) => {
+    return {
+        type:"REMOVE_QUOTE",
+        quoteId: quoteToRemove.id
+    }
+}
+
+export const upvoteQuote = (quote) => {
+    return {
+        type:"UPVOTE_QUOTE",
+        quoteId: quote.id
+    }
+}
+
+export const downvoteQuote = (quote) => {
+    return {
+        type:"DOWNVOTE_QUOTE",
+        quoteId: quote.id
+    }
+}
+
